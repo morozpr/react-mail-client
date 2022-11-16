34ecf5dfb58edcd88c51b4aacd23a7cea1feb1ea
morozpr <mihsa372004@gmail.com>
2022-11-16 09:28:01 +0300

    emailSend

---
+++ src/components/Menu.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+const Menu = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_yy9753g', 'template_d9hp4mu', form.current, 'SgCWbbBtjBcG-Xn2M')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+
+        }
+
+    return (
+        <form className='menu' ref={form} onSubmit={ sendEmail }>
+            <input name='sender' className='sender' placeholder='enter your email address' />
+            <input name='reciever' className='reciever' placeholder='enter reciever email address' />
+            <input name='subject' className='subject' placeholder='enter title' />
+            <textarea name='message' className='body' placeholder='enter your text' />
+            <input type="submit" />
+        </form>
+    )
+}
+
+export default Menu;
