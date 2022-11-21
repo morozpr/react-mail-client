5dbfc6d642dae8c50ae4f2a62f6865c3b5bb8267
morozpr <mihsa372004@gmail.com>
2022-11-21 17:59:50 +0300

    style

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -11,19 +11,27 @@ const Menu = () => {
         emailjs.sendForm('service_yy9753g', 'template_d9hp4mu', form.current, 'SgCWbbBtjBcG-Xn2M')
           .then((result) => {
               console.log(result.text);
+              alert('ПИСЬМО УСПЕШНО ОТПРАВЛЕНО');
           }, (error) => {
               console.log(error.text);
+              alert('ВНИМАНИЕ! ПРОИЗОШЛА НЕПРЕДВИДЕННАЯ ОШИБКА');
           });
 
         }
 
     return (
         <form className='menu' ref={form} onSubmit={ sendEmail }>
+            <div className='ema'>
             <input name='sender' className='sender' placeholder='enter your email address' />
             <input name='reciever' className='reciever' placeholder='enter reciever email address' />
             <input name='subject' className='subject' placeholder='enter title' />
+            </div>
+            <div className='text'>
             <textarea name='message' className='body' placeholder='enter your text' />
-            <input type="submit" />
+            </div>
+            <div className='botom'>
+            <input type="submit" className='bot'/>
+            </div>
         </form>
     )
 }
